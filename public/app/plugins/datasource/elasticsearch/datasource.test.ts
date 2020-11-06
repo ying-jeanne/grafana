7ef9ebc223b3fefda7b7ce24046722c45de663d0
Elfo404 <gio.ricci@grafana.com>
2020-11-06 16:03:22 +0000

    Fix wrong rebase resolution & restore deleted tests

--- public/app/plugins/datasource/elasticsearch/datasource.test.ts
+++ public/app/plugins/datasource/elasticsearch/datasource.test.ts
@@ -386,6 +386,7 @@ describe('ElasticDatasource', function(this: any) {
                   properties: {
                     '@timestamp': { type: 'date' },
                     __timestamp: { type: 'date' },
+                    '@timestampnano': { type: 'date_nanos' },
                     beat: {
                       properties: {
                         name: {
@@ -432,6 +433,7 @@ describe('ElasticDatasource', function(this: any) {
       expect(fields).toEqual([
         '@timestamp',
         '__timestamp',
+        '@timestampnano',
         'beat.name.raw',
         'beat.name',
         'beat.hostname',
@@ -455,7 +457,7 @@ describe('ElasticDatasource', function(this: any) {
 
       const fields = _.map(fieldObjects, 'text');
 
-      expect(fields).toEqual(['@timestamp', '__timestamp']);
+      expect(fields).toEqual(['@timestamp', '__timestamp', '@timestampnano']);
     });
   });
 
